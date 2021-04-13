f19028c42a024dd52f7671770b0cba6825a4c2c9
HP <avinashtiwary6300@gmail.com>
2021-04-13 15:48:58 +0530

    'try_catch_added'

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -10,27 +10,34 @@ class WorldTime
 
   WorldTime({this.location,this.flag,this.url});
 
+
   Future<void> getTime() async {
-    Response response = await get(new Uri.https("worldtimeapi.org", '/api/timezone/$url')); ;
-    // print(response.body);
-    Map data = jsonDecode(response.body);
-    // print(data);
-    // print(data['dst']);
-    //get property from data
+    try{
+      Response response = await get(new Uri.https("worldtimeapi.org", '/api/timezone/$url')); ;
+      // print(response.body);
+      Map data = jsonDecode(response.body);
+      // print(data);
+      // print(data['dst']);
+      //get property from data
 
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-    String min=data['utc_offset'].substring(4,6);
-    //print(datetime);
-    print(offset);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+      String min=data['utc_offset'].substring(4,6);
+      //print(datetime);
+      print(offset);
 
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset),minutes: int.parse(min)));
-    //
-    // print(now);
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset),minutes: int.parse(min)));
+      //
+      // print(now);
 
-    time=now.toString();
+      time=now.toString();
+    }
+    catch(e){
+      print('Error message $e');
+    }
+
   }
 
 }
