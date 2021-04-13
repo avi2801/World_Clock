b0fe93741e4fa5d4128325284ad86c40bbb680a9
HP <avinashtiwary6300@gmail.com>
2021-04-13 12:50:05 +0530

    time added function

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,38 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' ;
+import 'package:http/http.dart';
 import 'dart:convert';
 
 class Loading extends StatefulWidget {
-
   @override
   _LoadingState createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
-  String url='https://jsonplaceholder.typicode.com/todos/1';
-  void getData() async
-  {
-     Response response= await get(Uri.https('jsonplaceholder.typicode.com', 'todos/1'));
-     print(response.body);
-     Map data = jsonDecode(response.body);
-     print(data['title']);
+
+  void getData() async {
+
+    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Kolkata'));
+    // print(response.body);
+    Map data = jsonDecode(response.body);
+    // print(data);
+    // print(data['dst']);
+    //get property from data
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    //print(datetime);
+    //print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
-
-
-
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getData();
   }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(body:Text('hi'),
-      ),
+    return Scaffold(
+      body: Text('loading screen'),
     );
   }
 }
-
